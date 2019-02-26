7438fe8a44437cca03b669bb8a79c362b2a92c7f
Nicole Hategekimana <nicoleha127@gmail.com>
2019-02-26 16:00:50 +0200

    project initialised

--- app/models.py
+++ app/models.py
@@ -1,44 +1,42 @@
-class Movie:
-    '''
-    Movie class to define Movie Objects
-    '''
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
-    def __init__(self,id,title,overview,poster,vote_average,vote_count):
-        self.id =id
-        self.title = title
-        self.overview = overview
-        self.poster = "https://image.tmdb.org/t/p/w500/" + poster
-        self.vote_average = vote_average
-        self.vote_count = vote_count
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    comment_id = db.Column(db.Integer,db.ForeignKey('comments.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
 
-
-class Review:
-
-    all_reviews = []
-
-    def __init__(self,movie_id,title,imageurl,review):
-        self.movie_id = movie_id
-        self.title = title
-        self.imageurl = imageurl
-        self.review = review
-
-
-    def save_review(self):
-        Review.all_reviews.append(self)
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
 
 
-    @classmethod
-    def clear_reviews(cls):
-        Review.all_reviews.clear()
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
-    @classmethod
-    def get_reviews(cls,id):
+    def __repr__(self):
+        return f'User {self.username}'
 
-        response = []
+class Comment(db.Model):
+    __tablename__ = 'comments'
 
-        for review in cls.all_reviews:
-            if review.movie_id == id:
-                response.append(review)
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
 
-        return response
+    def __repr__(self):
+        return f'User {self.comment}'
