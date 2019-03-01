0a33ee826ac24e2051da3d94cec66047ecdebdd2
Nicole Hategekimana <nicoleha127@gmail.com>
2019-03-01 18:30:44 +0200

    database created

--- app/request.py
+++ app/request.py
@@ -1,5 +1,16 @@
 from .models import Quote
 import urllib.request,json
 
-base_url=None
+base_url=app.config['QUOTE_API_BASE_URL']
+def get_quote():
+    '''
+    function that gets the json response to url request
+    '''
+    get_quote_url=base_url.format()
+    with urllib.request.urlopen(get_quote_url) as url:
+        get_quote_data=url.read()
+        get_quote_response=json.loads(get_quote_data)
+
+        quote_objects= None
+    return source_objects    
 
