6b192110a6f47d94d67e153494d9e93811b23c14
Nicole Hategekimana <nicoleha127@gmail.com>
2019-03-04 19:23:53 +0200

    deployment to heroku

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 class ProdConfig(Config):
-    pass
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL_TEST')
 
